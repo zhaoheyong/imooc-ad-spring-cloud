274c3f3e493e9f9c6b35b7506620573c335e4c9d
zhaoheyong <zhaoheyong1992@163.com>
2019-06-25 08:28:55 +0800

    1

--- imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/impl/UserServiceImpl.java
+++ imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/service/impl/UserServiceImpl.java
@@ -6,15 +6,22 @@ import com.imooc.ad.vo.CreateUserRequest;
 import com.imooc.ad.vo.CreateUserResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Qinyi.
  */
 @Slf4j
 @Service
-public class UserServiceImpl implements IUserService {@Override
+public class UserServiceImpl implements IUserService {
+	
+	@Override
+	@Transactional
 	public CreateUserResponse createUser(CreateUserRequest request) throws AdException {
 		// TODO Auto-generated method stub
+		if(!request.validate()) {
+			throw new AdException("");
+		}
 		return null;
 	}
 
